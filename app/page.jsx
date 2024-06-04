e6336ffeb39d1c2874343fdb3dd6052d5f350deb
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2024-06-04 15:17:21 +0200

    feat: add states to handle settings and add first image preview

--- app/page.jsx
+++ app/page.jsx
@@ -5,8 +5,19 @@ import LabelPrimary from "../src/components/labels/LabelPrimary";
 
 export default function Home() {
 
-  const [settings, setSettings] = useState()
+  const [settings, setSettings] = useState({
+    padding: 16,
+    shadow: 4,
+    radius: 8,
+  })
   const [image, setImage] = useState()
+
+  const setSetting = (name, value) => {
+    setSettings((curr) => ({
+      ...curr,
+      [name]: value
+    }))
+  }
 
   const handleFileChange = (e) => {
     const files = e.target.files
@@ -30,35 +41,38 @@ export default function Home() {
     reader.readAsDataURL(file)
   }
 
-  console.log(image);
 
 
+  return (
+    <main className="w-full flex justify-center items-center max-w-4xl px-4 py-8 max-lg:flex-col m-auto gap-8 lg:gap-16 min-h-full text-black">
+      <div className="card max-w-lg flex-1 bg-base-200 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Settings</h2>
+          <LabelPrimary title="File">
+            <input type="file"
+              className="file-input file-input-primary file-input-bordered file-input-sm w-full max-w-xs"
+              onChange={(e) => handleFileChange(e)} />
 
-return (
-  <main className="w-full flex justify-center items-center max-w-4xl px-4 py-8 max-lg:flex-col m-auto gap-8 lg:gap-16 min-h-full text-black">
-    <div className="card max-w-lg flex-1 bg-base-200 shadow-xl">
-      <div className="card-body">
-        <h2 className="card-title">Settings</h2>
-        <LabelPrimary title="File">
-          <input type="file"
-            className="file-input file-input-primary file-input-bordered file-input-sm w-full max-w-xs"
-            onChange={(e) => handleFileChange(e)} />
 
+          </LabelPrimary>
+          <LabelPrimary title="Padding">
+            <input type="range" min={0} max="100" value={settings.padding} className="range range-sm range-primary"
+              onChange={(e) => setSetting("padding", e.target.value)} />
+          </LabelPrimary>
+          <LabelPrimary title="Shadow">
+            <input type="range" min={0} max="100" value={settings.shadow} className="range range-sm range-primary"
+              onChange={(e) => setSetting("shadow", e.target.value)} />
+          </LabelPrimary>
+          <LabelPrimary title="Radius">
+            <input type="range" min={0} max="100" value={settings.radius} className="range range-sm range-primary"
+              onChange={(e) => setSetting("radius", e.target.value)} />
+          </LabelPrimary>
+        </div>
 
-        </LabelPrimary>
-        <LabelPrimary title="Padding">
-          <input type="range" min={0} max="100" defaultValue="50" className="range range-sm range-primary" />
-        </LabelPrimary>
-        <LabelPrimary title="Shadow">
-          <input type="range" min={0} max="100" defaultValue="50" className="range range-sm range-primary" />
-        </LabelPrimary>
-        <LabelPrimary title="Radius">
-          <input type="range" min={0} max="100" defaultValue="50" className="range range-sm range-primary" />
-        </LabelPrimary>
       </div>
-
-    </div>
-    <div>preview</div>
-  </main>
-);
+      <div className="max-w-sm">
+        {image && <img src={image.src} alt={image.name} className="w-full h-fit" />}
+      </div>
+    </main>
+  );
 }
