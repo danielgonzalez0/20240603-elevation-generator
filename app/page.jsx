9141a6d6595636dc20812a18b1ef46c15c793c6a
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2024-06-04 12:41:34 +0200

    feat: end installation

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function Home() {
+  return (
+    <main className="w-full flex justify-center items-center max-w-4xl px-4 py-8 max-lg:flex-col m-auto gap-8 lg:gap-16 min-h-full text-black">
+      <div>card</div>
+      <div>preview</div>
+    </main>
+  );
+}
